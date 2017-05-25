4032e2a85d6a06c04a3170131c5f3edd8c8f33f4
1501144 <gcmcf057@gmail.com>
2017-05-25 12:01:56 +0900

    メイン画面

--- app/src/main/java/jp/ac/asojuku/jousenb/markofriss/MainActivity.java
+++ app/src/main/java/jp/ac/asojuku/jousenb/markofriss/MainActivity.java
@@ -1,7 +1,11 @@
 package jp.ac.asojuku.jousenb.markofriss;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnPicUp = ( Button ) this.findViewById ( R.id.button );
+
+        btnPicUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, genre.class);
+
+                startActivity(intent);
+
+            }
+        });
     }
 }
