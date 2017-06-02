8ea83ba0b2b24285e9bdc8345bd28c927f2e2359
hide <1501153@st.asojuku.ac.jp>
2017-06-02 12:42:26 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/vcs.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/jp/ac/asojuku/jousenb/markofriss/years.java
+++ app/src/main/java/jp/ac/asojuku/jousenb/markofriss/years.java
@@ -1,37 +1,34 @@
 package jp.ac.asojuku.jousenb.markofriss;
 
-import java.util.ArrayList;
 
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
-import static android.R.attr.id;
-import static android.R.id.list;
-
 public class years extends AppCompatActivity {
+
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_years);
 
+        ListView listView = (ListView) findViewById(R.id.listview1);
+
         //Listの作成
-        ArrayList<String> list = new ArrayList<>();
-        // アイテムを追加します
-        list.add("hide");
-        list.add("ひで");
-        list.add("HIDE");
+        ArrayList<String> items = new ArrayList<>();
+        // データを準備
+        items.add("hide");
+        items.add("ひで");
+        items.add("HIDE");
 
         //Adapterの作成
         //第二引数のレイアウトにandroid付属のレイアウトを指定する
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
 
-        // ListViewにAdapterを関連付ける
-        ListView listView = (ListView) findViewById(R.id.listview1);
+        // ListViewに表示
         listView.setAdapter(adapter);
     }
 }
