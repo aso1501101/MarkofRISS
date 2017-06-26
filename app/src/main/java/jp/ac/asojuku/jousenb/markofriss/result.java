0215713b27cd2acf390bc12e4202765665673667
kotaro <1501101@st.asojuku.ac.jp>
2017-06-26 15:10:21 +0900

    wwww

--- app/src/main/java/jp/ac/asojuku/jousenb/markofriss/result.java
+++ app/src/main/java/jp/ac/asojuku/jousenb/markofriss/result.java
@@ -3,6 +3,8 @@ package jp.ac.asojuku.jousenb.markofriss;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class result extends AppCompatActivity {
@@ -34,5 +36,15 @@ public class result extends AppCompatActivity {
 
         TextView tv2 = (TextView)findViewById(R.id.textViewall);
         tv2.setText(counta);
+
+        Button btntop = (Button)this.findViewById(R.id.btntop);
+        btntop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(result.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
