2d947ceb775215737bf389e295e7c6e44d38c8df
kotaro <1501101@st.asojuku.ac.jp>
2017-06-26 15:02:26 +0900

    wwww

--- app/src/main/java/jp/ac/asojuku/jousenb/markofriss/result.java
+++ app/src/main/java/jp/ac/asojuku/jousenb/markofriss/result.java
@@ -1,7 +1,9 @@
 package jp.ac.asojuku.jousenb.markofriss;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class result extends AppCompatActivity {
 
@@ -9,5 +11,28 @@ public class result extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        String counts = "";
+        String counta = "";
+
+        Intent intent = getIntent();
+        counts = intent.getStringExtra("counts");
+        counta = intent.getStringExtra("count");
+
+        int countse = Integer.parseInt(counta);
+        countse = countse - 1;
+        counta = String.valueOf(countse);
+
+
+        TextView tv = (TextView)findViewById(R.id.textViewexact);
+        tv.setText(counts);
+
+        TextView tv2 = (TextView)findViewById(R.id.textViewall);
+        tv2.setText(counta);
     }
 }
