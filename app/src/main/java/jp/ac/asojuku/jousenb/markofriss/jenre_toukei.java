ca5ddbd2113612e0257e0ccf61417b330dea56c9
et <1501102@st.asojuku.ac.jp>
2017-07-09 01:59:22 +0900

    こみこみこみこみみみこみもk

--- app/src/main/java/jp/ac/asojuku/jousenb/markofriss/jenre_toukei.java
+++ app/src/main/java/jp/ac/asojuku/jousenb/markofriss/jenre_toukei.java
@@ -1,9 +1,16 @@
 package jp.ac.asojuku.jousenb.markofriss;
 
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class jenre_toukei extends AppCompatActivity {
+
+    private SQLiteDatabase sqlDB;
+    DBManager2 dbm2;
+    String kaitou1 = "";
+    String seikai = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +21,11 @@ public class jenre_toukei extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        setContentView(R.layout.activity_jenre_toukei);
+
+        Intent intent = getIntent();
+
+
     }
 }
 
