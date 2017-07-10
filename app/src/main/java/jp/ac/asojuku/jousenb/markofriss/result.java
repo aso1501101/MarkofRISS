ee414caa83c44783e053b56da956b81520f43c6e
kotaro <1501101@st.asojuku.ac.jp>
2017-07-10 14:59:26 +0900

    wwww

--- app/src/main/java/jp/ac/asojuku/jousenb/markofriss/result.java
+++ app/src/main/java/jp/ac/asojuku/jousenb/markofriss/result.java
@@ -3,9 +3,13 @@ package jp.ac.asojuku.jousenb.markofriss;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class result extends AppCompatActivity {
 
@@ -13,6 +17,24 @@ public class result extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        LinearLayout cardLinear = (LinearLayout)this.findViewById(R.id.cardLinear);
+        cardLinear.removeAllViews();
+
+        for (int i = 0; i < 5; i++) {
+            LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+                LinearLayout linearLayout = (LinearLayout)inflater.inflate(R.layout.test_card,null);
+                CardView cardView = (CardView) linearLayout.findViewById(R.id.cardView);
+                TextView textBox = (TextView) linearLayout.findViewById(R.id.textBox);
+                textBox.setText("CardView" + i);
+                cardView.setTag(i);
+                cardView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(result.this, String.valueOf(v.getTag()) + "番目のCardViewがクリックされました。", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                cardLinear.addView(linearLayout,i);
+        }
     }
 
     @Override
